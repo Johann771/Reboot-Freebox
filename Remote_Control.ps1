4216409f1f60ba8c72778b516b531ab7ce8a3de5
Johann Weytens <johann.weytens@gmail.com>
2024-10-02 21:09:55 +0200

    edit to work on linux

--- Remote_Control.ps1
+++ Remote_Control.ps1
@@ -1,4 +1,11 @@
-﻿#######################################################################################################################
+﻿#!/usr/bin/pwsh
+
+# Remote_Control adapted to work with PowerShell Core on Linux
+
+# Thanks to My Canaletto for the script
+# https://www.canaletto.fr/post/surveiller-et-redemarrer-une-freebox-a-distance
+
+###############################################
 #
 #  Remote_Control
 #
@@ -47,87 +54,84 @@ $JetonAppli = "[Insérer l'identifiant d'application ici]"
 
 # Lecture de l'action demandée
 if ($args[0] -ieq "-info") {
-  $Action = "Infos"
+    $Action = "Infos"
 } elseif ($args[0] -ieq "-reboot") {
-  $Action = "Reboot"
+    $Action = "Reboot"
 } else {
-  "Paramètre requis manquant ou incorrect."
-  Remove-Variable -Name "AdresseFreebox"
-  Remove-Variable -Name "JetonAppli"
-  exit
+    Write-Output "Paramètre requis manquant ou incorrect."
+    Remove-Variable -Name "AdresseFreebox" -ErrorAction SilentlyContinue
+    Remove-Variable -Name "JetonAppli" -ErrorAction SilentlyContinue
+    exit
 }
 
 # Récupération des informations de la Freebox
 $FbxApiInfo = Invoke-RestMethod -Uri "$AdresseFreebox/api_version"
 
 # Stocker l'URL de base des appels
-$BaseUrl = "$AdresseFreebox$($FbxApiInfo.api_base_url)v$([int]$FbxApiInfo.api_version)"
+$BaseUrl = "$AdresseFreebox$($FbxApiInfo.api_base_url)v$($FbxApiInfo.api_version -as [int])"
 
 # Préparer l'ouverture de session
 $Challenge = (Invoke-RestMethod -Uri "$BaseUrl/login").result.challenge
-$hmacsha = New-Object System.Security.Cryptography.HMACSHA1
-$hmacsha.key = [Text.Encoding]::ASCII.GetBytes($JetonAppli)
-$signature = $hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes($Challenge))
-$password = [string]::join("", ($signature | % {([int]$_).toString('x2')}))
+$hmacsha = [System.Security.Cryptography.HMACSHA1]::new()
+$hmacsha.key = [System.Text.Encoding]::ASCII.GetBytes($JetonAppli)
+$signature = $hmacsha.ComputeHash([System.Text.Encoding]::ASCII.GetBytes($Challenge))
+$password = -join ($signature | ForEach-Object { $_.ToString('x2') })
 $SessionJson = @"
 {
    `"app_id`": `"fr.freebox.remotecontrol`",
-   `"password`": `"$($password)`"
+   `"password`": `"$password`"
 }
 "@
 
 # Ouvrir la session
-$Session = Invoke-RestMethod -Uri "$BaseUrl/login/session/" -Method Post -Body $SessionJson
+$Session = Invoke-RestMethod -Uri "$BaseUrl/login/session/" -Method Post -Body $SessionJson -ContentType "application/json"
 
 # Supprimer les variables désormais inutiles
-Remove-Variable -Name "JetonAppli"
-Remove-Variable -Name "FbxApiInfo"
-Remove-Variable -Name "Challenge"
-Remove-Variable -Name "hmacsha"
-Remove-Variable -Name "signature"
-Remove-Variable -Name "password"
-Remove-Variable -Name "SessionJson"
+Remove-Variable -Name "JetonAppli" -ErrorAction SilentlyContinue
+Remove-Variable -Name "FbxApiInfo" -ErrorAction SilentlyContinue
+Remove-Variable -Name "Challenge" -ErrorAction SilentlyContinue
+Remove-Variable -Name "hmacsha" -ErrorAction SilentlyContinue
+Remove-Variable -Name "signature" -ErrorAction SilentlyContinue
+Remove-Variable -Name "password" -ErrorAction SilentlyContinue
+Remove-Variable -Name "SessionJson" -ErrorAction SilentlyContinue
 
 if ($Session.success) {
-  $JetonSession = $Session.result.session_token;
+    $JetonSession = $Session.result.session_token
 } else {
-  "Echec de l'ouverture de session."
-  Remove-Variable -Name "Session"
-  Remove-Variable -Name "BaseUrl"
-  Remove-Variable -Name "Action"
-  Remove-Variable -Name "AdresseFreebox"
-  exit
+    Write-Output "Echec de l'ouverture de session."
+    Remove-Variable -Name "Session" -ErrorAction SilentlyContinue
+    Remove-Variable -Name "BaseUrl" -ErrorAction SilentlyContinue
+    Remove-Variable -Name "Action" -ErrorAction SilentlyContinue
+    Remove-Variable -Name "AdresseFreebox" -ErrorAction SilentlyContinue
+    exit
 }
 
 # Construire l'entête de session
-$EnteteSession = @{'X-Fbx-App-Auth' = $($JetonSession)}
-Remove-Variable -Name "JetonSession"
+$EnteteSession = @{ 'X-Fbx-App-Auth' = $JetonSession }
+Remove-Variable -Name "JetonSession" -ErrorAction SilentlyContinue
 
 # Exécuter l'action demandée
 if ($Action -eq "Infos") {
-  $Reponse = Invoke-RestMethod -Uri "$BaseUrl/system/" -Headers $EnteteSession
-  ConvertTo-Json $Reponse.result
+    $Reponse = Invoke-RestMethod -Uri "$BaseUrl/system/" -Headers $EnteteSession
+    $Reponse.result | ConvertTo-Json
 } elseif ($Action -eq "Reboot") {
-  $Reponse = Invoke-RestMethod -Uri "$BaseUrl/system/reboot/" -Method Post -Headers $EnteteSession
-  #$Reponse = Invoke-RestMethod -Uri "https://mafreebox.freebox.fr/api/v4/system/reboot/" -Method Post -Headers $EnteteSession
-  if ($Reponse.success) {
-    "La Freebox est en train de rebooter."
-  } else {
-    "Reboot de la Freebox impossible: {0}" -f $Reponse.msg
-  }
+    $Reponse = Invoke-RestMethod -Uri "$BaseUrl/system/reboot/" -Method Post -Headers $EnteteSession
+    if ($Reponse.success) {
+        Write-Output "La Freebox est en train de rebooter."
+    } else {
+        Write-Output ("Reboot de la Freebox impossible: {0}" -f $Reponse.msg)
+    }
 } else {
-  $Reponse = "Action inconnue"
-  $Reponse
+    $Reponse = "Action inconnue"
+    Write-Output $Reponse
 }
 
 # Supprimer les variables restantes
-Remove-Variable -Name "Reponse"
-Remove-Variable -Name "EnteteSession"
-Remove-Variable -Name "Session"
-Remove-Variable -Name "BaseUrl"
-Remove-Variable -Name "Action"
-Remove-Variable -Name "AdresseFreebox"
-
-
+Remove-Variable -Name "Reponse" -ErrorAction SilentlyContinue
+Remove-Variable -Name "EnteteSession" -ErrorAction SilentlyContinue
+Remove-Variable -Name "Session" -ErrorAction SilentlyContinue
+Remove-Variable -Name "BaseUrl" -ErrorAction SilentlyContinue
+Remove-Variable -Name "Action" -ErrorAction SilentlyContinue
+Remove-Variable -Name "AdresseFreebox" -ErrorAction SilentlyContinue
 
 # EOF
